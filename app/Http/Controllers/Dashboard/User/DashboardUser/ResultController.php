f738ae79b79192fad2fae94955668008a36d2403
PandhuWibowo <pandhuw58@gmail.com>
2019-01-12 23:45:21 +0700

    skoring selesai di bagian user

--- app/Http/Controllers/Dashboard/User/DashboardUser/ResultController.php
+++ app/Http/Controllers/Dashboard/User/DashboardUser/ResultController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Redirect;
 use App\Http\Controllers\Controller;
 // use App\Http\Models\User;
 // use App\Http\Models\JenisAssesment;
-// use App\Http\Models\Assesment;
+use App\Http\Models\AssessmentKompetensi;
 use App\Http\Models\RowScore;
 use Illuminate\Support\Facades\Crypt;
 use Carbon\Carbon;
@@ -25,7 +25,7 @@ class ResultController extends Controller
   public function show($assId){
     $assId  = Crypt::decrypt($assId);
     $query  = DB::table("pertanyaan_assesments as pa")
-                ->select("r.id as rId","r.nama_rowscore as namaRowScore","k.kompetensi as kKompetensi",DB::raw("SUM(pa.nilai) as sum_nilai"))
+                ->select("k.id as kId","r.id as rId","r.nama_rowscore as namaRowScore","k.kompetensi as kKompetensi",DB::raw("SUM(pa.nilai) as sum_nilai"))
                 ->join("pertanyaans as p","pa.pertanyaan_id","=","p.id")
                 ->join("jawabans as j","pa.jawaban_id","=","j.id")
                 ->join("rowscores as r","p.rowscore_id","=","r.id")
@@ -35,7 +35,7 @@ class ResultController extends Controller
                 ->groupBy("r.no_urut_rowscore","k.no_urut_kompetensi")
                 ->get();
   $query2   = DB::table("pertanyaan_assesments as pa")
-              ->select("r.id as rId","r.nama_rowscore as namaRowScore","k.kompetensi as kKompetensi",DB::raw("SUM(pa.nilai) as sum_nilai"))
+              ->select("k.id as kId","r.id as rId","r.nama_rowscore as namaRowScore","k.kompetensi as kKompetensi",DB::raw("SUM(pa.nilai) as sum_nilai"))
               ->join("pertanyaans as p","pa.pertanyaan_id","=","p.id")
               ->join("jawabans as j","pa.jawaban_id","=","j.id")
               ->join("rowscores as r","p.rowscore_id","=","r.id")
@@ -46,7 +46,75 @@ class ResultController extends Controller
               ->get();
 
 
-    $rowscores = RowScore::all();
+              foreach($query2 as $row){
+                $nilai_assess;
+                $nilai_sjq = $row->sum_nilai * 0.4;
+                // <!-- sum_nilai sjq = {{$row->sum_nilai}} <br /> -> ini juga -->
+
+                // <!-- ngeloop assessment -->
+                foreach($query as $row2){
+                  if($row->kKompetensi == $row2->kKompetensi){
+                    $nilai_assess = ($row2->sum_nilai / 4)*0.6;
+                    // <!-- sum_nilai assessment = {{$row2->sum_nilai}}<br /> -> ini kan cuma setelah diitung masing2 -->
+                  }
+                }
+
+                // <!-- ngitung total -->
+                // <!-- assessment = {{$nilai_assess}}, sjq = {{$nilai_sjq}}<br /> -> trus ini hasil tambah -->
+                $nilai_total = $nilai_assess + $nilai_sjq;
+                $hasil[$row->kKompetensi] = $nilai_total;
+                // <!-- {{$hasil[$row->kKompetensi]}} -->
+                if($hasil[$row->kKompetensi] >= 3.75 && $hasil[$row->kKompetensi] <= 4.75){
+                  $kompetensi4 = new AssessmentKompetensi;
+                  $kompetensi4->id            = Uuid::generate()->string;
+                  $kompetensi4->ass_id        = $assId;
+                  $kompetensi4->kompetensi_id = $row->kId;
+                  $kompetensi4->weight        = $hasil[$row->kKompetensi];
+                  $kompetensi4->pembulatan    = 4;
+                  $kompetensi4->save();
+                  // echo $row->kId."-".$row->kKompetensi."- 4 <br/>";
+                }else if($hasil[$row->kKompetensi] >= 2.75 && $hasil[$row->kKompetensi] < 3.75){
+                  $kompetensi3 = new AssessmentKompetensi;
+                  $kompetensi3->id            = Uuid::generate()->string;
+                  $kompetensi3->ass_id        = $assId;
+                  $kompetensi3->kompetensi_id = $row->kId;
+                  $kompetensi3->weight        = $hasil[$row->kKompetensi];
+                  $kompetensi3->pembulatan    = 3;
+                  $kompetensi3->save();
+                  // echo $row->kId."-".$row->kKompetensi."- 3 <br/>";
+                }else if($hasil[$row->kKompetensi] >= 1.75 && $hasil[$row->kKompetensi] < 2.75){
+                  $kompetensi2 = new AssessmentKompetensi;
+                  $kompetensi2->id            = Uuid::generate()->string;
+                  $kompetensi2->ass_id        = $assId;
+                  $kompetensi2->kompetensi_id = $row->kId;
+                  $kompetensi2->weight        = $hasil[$row->kKompetensi];
+                  $kompetensi2->pembulatan    = 2;
+                  $kompetensi2->save();
+                  // echo $row->kId."-".$row->kKompetensi."- 2 <br/>";
+                }else if($hasil[$row->kKompetensi] >= 0.75 && $hasil[$row->kKompetensi] < 1.75){
+                  $kompetensi1 = new AssessmentKompetensi;
+                  $kompetensi1->id            = Uuid::generate()->string;
+                  $kompetensi1->ass_id        = $assId;
+                  $kompetensi1->kompetensi_id = $row->kId;
+                  $kompetensi1->weight        = $hasil[$row->kKompetensi];
+                  $kompetensi1->pembulatan    = 1;
+                  $kompetensi1->save();
+                  // echo $row->kId."-".$row->kKompetensi."- 1 <br/>";
+                }else{
+                  $kompetensi0 = new AssessmentKompetensi;
+                  $kompetensi0->id            = Uuid::generate()->string;
+                  $kompetensi0->ass_id        = $assId;
+                  $kompetensi0->kompetensi_id = $row->kId;
+                  $kompetensi0->weight        = $hasil[$row->kKompetensi];
+                  $kompetensi0->pembulatan    = 0;
+                  $kompetensi0->save();
+                  // echo $row->kId."-".$row->kKompetensi."- 0 <br/>";
+                }
+                // $kompetensiFinal->save();
+              }
+
+    // echo dd($query3);
+    // $rowscores = RowScore::all();
     return view("partisipan.dashboard.result.v_index", compact("query","rowscores","query2"));
   }
 }
---
+++ app/Http/Models/AssessmentKompetensi.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class AssessmentKompetensi extends Model{
+  use SoftDeletes;
+  protected $fillable = ['id','ass_id','kompetensi_id','weight','pembulatan'];
+  protected $table = "assesment_kompetensis";
+  protected $primaryKey = 'id'; // or null
+  public $incrementing = false;
+
+  // public function get_assesment(){
+  //     return $this->belongsTo(JenisAssesment::class,'assesment_id');
+  // }
+  //
+  // public function get_kompetensi(){
+  //   return $this->belongsTo(Kompetensi::class, 'kompetensi_id');
+  // }
+  //
+  // public function get_rowscore(){
+  //   return $this->belongsTo(RowScore::class, 'rowscore_id');
+  // }
+  //
+  // public function data_jawabans(){
+  //   return $this->hasMany('App\Http\Models\Jawaban');
+  // }
+  //
+  // public function get_jawaban(){
+  //   return $this->hasMany(Jawaban::class);
+  // }
+}
