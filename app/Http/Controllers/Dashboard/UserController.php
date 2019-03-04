070475b20ab8f90d8555b216621fafbaa17f7407
PandhuWibowo <pandhuw58@gmail.com>
2019-03-04 18:16:43 +0700

    lagi ngerjain log bagian participant

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -13,26 +13,64 @@ use Illuminate\Support\Facades\Session;
 use Webpatser\Uuid\Uuid;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Models\ModelLogs\DirectPage;
+use App\Http\Models\ModelLogs\Participant;
+use BrowserDetect;
+
 /**
  * AdministratorController
  */
 class UserController extends Controller
 {
 
-    public function index(){
+    public function index(Request $request){
+      $logPages = new DirectPage([
+        "user_id"     => Session::get("id"),
+        "ip_address"  => $request->ip(),
+        "browser"     => BrowserDetect::browserName(),
+        "action"      => "Menu User",
+        "data"        => Session::get("email")." mengunjungi halaman User",
+        "link"        => url()->current()
+      ]);
+
+      $logPages->save();
+
       $dataUsers = User::where('level', 'Participant')->orderBy('firstname', 'desc')->get();
       return view("administrator.dashboard.pages.user-master-page.v_index", compact('dataUsers'));
     }
 
-    public function show($id){
+    public function show($id, Request $request){
       $decryptId            = Crypt::decrypt($id);
+
+      $logPages = new DirectPage([
+        "user_id"     => Session::get("id"),
+        "ip_address"  => $request->ip(),
+        "browser"     => BrowserDetect::browserName(),
+        "action"      => "Detail User",
+        "data"        => Session::get("email")." mengunjungi halaman Detail User dengan No. Id : ".$id,
+        "link"        => url()->current()
+      ]);
+
+      $logPages->save();
+
       $dataUsers            = User::where('level','Participant')->where('id',$decryptId)->first();
       // echo response()->json($detailAdministrator);
       return view("administrator.dashboard.pages.user-master-page.detail.v_view", compact('dataUsers'));
       // echo $decryptId;
     }
 
-    public function add(){
+    public function add(Request $request){
+      $logPages = new DirectPage([
+        "user_id"     => Session::get("id"),
+        "ip_address"  => $request->ip(),
+        "browser"     => BrowserDetect::browserName(),
+        "action"      => "Add New User",
+        "data"        => Session::get("email")." mengunjungi halaman Add New User",
+        "link"        => url()->current()
+      ]);
+
+      $logPages->save();
+
       return view("administrator.dashboard.pages.user-master-page.v_add_user");
     }
 
@@ -163,11 +201,46 @@ class UserController extends Controller
 
     public function destroy(Request $request){
       $txtId    = Crypt::decrypt($request->id);
-      User::where('id',$txtId)->delete();
-      return response()->json(
-        array(
-          'response'  => "success"
-        )
-      );
+
+      $data = User::where('id', $txtId)->where('active', '1')->first();
+
+      if(User::where('id',$txtId)->delete()){
+        $log = new Participant([
+          "user_id"     => Session::get("id"),
+          "ip_address"  => $request->ip(),
+          "browser"     => BrowserDetect::browserName(),
+          "action"      => "Delete User - Delete|Success",
+          "data"        => "Berhasil menghapus data User - User ID : ".$data->id.", Firstname : ".ucfirst(trim($data->firstname)).", Lastname : ".ucfirst(trim($data->lastname)).", Nickname : ".ucfirst(trim($data->nickname)).", Email : ".trim($data->email).", Username : ".ucfirst(trim($data->username)).", Phone Number : ".trim($data->phone).
+                           ", Level : ".trim($data->level).", Address : ".ucfirst(trim($data->address)).", Password : ".Hash::make($data->password).", Image : ".$data->image,
+          "link"        => url()->current()
+        ]);
+
+        $log->save();
+
+        return response()->json(
+          array(
+            'response'  => "success"
+          )
+        );
+      }else{
+        $log = new Participant([
+          "user_id"     => Session::get("id"),
+          "ip_address"  => $request->ip(),
+          "browser"     => BrowserDetect::browserName(),
+          "action"      => "Delete User - Delete|Failed",
+          "data"        => "Gagal menghapus data User - User ID : ".$data->id.", Firstname : ".ucfirst(trim($data->firstname)).", Lastname : ".ucfirst(trim($data->lastname)).", Nickname : ".ucfirst(trim($data->nickname)).", Email : ".trim($data->email).", Username : ".ucfirst(trim($data->username)).", Phone Number : ".trim($data->phone).
+                           ", Level : ".trim($data->level).", Address : ".ucfirst(trim($data->address)).", Password : ".Hash::make($data->password).", Image : ".$data->image,
+          "link"        => url()->current()
+        ]);
+
+        $log->save();
+
+        return response()->json(
+          array(
+            'response'  => "failed"
+          )
+        );
+      }
+
     }
 }
