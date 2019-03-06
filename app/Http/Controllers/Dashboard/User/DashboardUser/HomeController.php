fd080ac74197c1e4e1938502d3faa7704bca0892
PandhuWibowo <pandhuw58@gmail.com>
2019-03-06 17:17:32 +0700

    mulai menyelesaikan log users, home log sudah selesai

--- app/Http/Controllers/Dashboard/User/DashboardUser/HomeController.php
+++ app/Http/Controllers/Dashboard/User/DashboardUser/HomeController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Dashboard\User\DashboardUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\Controller;
+use App\Http\Models\ModelLogs\DirectPage;
+use BrowserDetect;
 
 /**
  *  HomeController
@@ -17,7 +20,17 @@ class HomeController extends Controller
   //     $this->middleware('auth');
   // }
 
-  public function index(){
+  public function index(Request $request){
+    $logPages = new DirectPage([
+      "user_id"     => Session::get("id"),
+      "ip_address"  => $request->ip(),
+      "browser"     => BrowserDetect::browserName(),
+      "action"      => "Menu Home",
+      "data"        => Session::get("email")." mengunjungi halaman Home",
+      "link"        => url()->current()
+    ]);
+
+    $logPages->save();
     // $title  = "Dashboard";
     return view('partisipan.dashboard.v_index');
   }
