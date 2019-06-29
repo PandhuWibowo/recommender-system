e7f5f3b4732ae35e9730fe11b485828aeb965ab6
PandhuWibowo <pandhuw58@gmail.com>
2019-06-29 23:52:56 +0700

    backup database

--- resources/views/administrator/dashboard/pages/logtest/v_index.blade.php
+++ resources/views/administrator/dashboard/pages/logtest/v_index.blade.php
@@ -133,7 +133,7 @@
                                 <div class="row">
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                         <div class="breadcome-heading">
-                                            
+
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
